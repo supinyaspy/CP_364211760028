fe45e42e2b748adb7c1b45b7661007a25387d5a0
ice <supinya.s@rmutsvmail.com>
2022-04-07 21:07:56 +0700

    assingment

---
+++ Lab1/เขียนโปรแกรม/assingment4/ass4_1.py
@@ -0,0 +1,15 @@
+# Assignment 4 Q1
+
+"""
+Name: {นางสาวสุภิญญา ศรีเจริญ}
+SID: {364211760028}
+Group: {MIT211}
+"""
+
+
+i = int(input("Enter number: "))
+count = 0
+while count < i:
+    if i % 5 == 0:
+        print(f'The result is : ', count)
+        count = count+5
---
+++ Lab1/เขียนโปรแกรม/assingment4/ass4_2.py
@@ -0,0 +1,15 @@
+# Assignment 4 Q2
+
+"""
+Name: {นางสาวสุภิญญา ศรีเจริญ}
+SID: {364211760028}
+Group: {MIT211}
+"""
+
+a = []
+while True:
+    x = int(input())
+    if x == 1:
+        break
+    a.append(x)
+print(a)
